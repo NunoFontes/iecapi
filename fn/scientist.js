651361d21475d30f5683926e026acd4ad09397e8
nunofontes <nuno@tradingeconomics.com>
2014-11-18 11:21:44 +0000

    state of that art

---
+++ fn/scientist.js
@@ -0,0 +1,57 @@
+exports.geoMapify = function(data,cb){
+  var distance_to_mean = 2.2
+  var array_of_the_values = []
+  var treated_data = []
+  data = _.sortBy(data,'LatestValue')
+ _.each(data,function(item){
+  treated_data.push({country:item.Country,category:item.Category,latestvalue:item.LatestValue,scale:item.LatestValue})
+  array_of_the_values.push(Number(item.LatestValue))
+ })
+
+
+ var stats = average(array_of_the_values)
+ console.log(stats)
+ var can_stop = true
+ for(var j = 0; can_stop && j<treated_data.length;j++){
+  if(withinStd(stats.mean, treated_data[j].scale, distance_to_mean*stats.deviation)){
+   can_stop = false
+  }else{
+   treated_data[j].scale = stats.mean-distance_to_mean*stats.deviation
+  }
+ }
+ 
+ can_stop = true
+ for(var j = treated_data.length-1; can_stop && j>0;j--){
+  if(withinStd(stats.mean, treated_data[j].scale, distance_to_mean*stats.deviation)){
+   can_stop = false
+  }else{
+   treated_data[j].scale = stats.mean+distance_to_mean*stats.deviation
+  }
+ }
+ treated_data.unshift({min_scale: treated_data[0].scale,max_scale: treated_data[treated_data.length-1].scale})
+ cb(treated_data)
+}
+
+
+exports.adjustify = function(data,cb){
+ 
+}
+
+exports.forecastify = function(data,cb){
+
+}
+
+
+var average = function(a) {
+  if(!a) return 1;
+  var r = {mean: 0, variance: 0, deviation: 0}, t = a.length;
+  for(var m, s = 0, l = t; l--; s += a[l]);
+  for(m = r.mean = s / t, l = t, s = 0; l--; s += Math.pow(a[l] - m, 2));
+  return r.deviation = Math.sqrt(r.variance = s / t), r;
+}
+    
+var withinStd = function(mean, val, stdev) {
+   var low = mean-(stdev);
+   var hi = mean+(stdev);
+   return (val > low) && (val < hi);
+}
